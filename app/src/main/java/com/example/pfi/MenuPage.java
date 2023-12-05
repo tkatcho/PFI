2907f87e10db5640d1ca16dcd18ff0d4a2aa1efc
elianarg <137467171+elianagonzalez01@users.noreply.github.com>
2023-12-05 16:33:34 -0500

    test

--- app/src/main/java/com/example/pfi/MenuPage.java
+++ app/src/main/java/com/example/pfi/MenuPage.java
@@ -1,10 +1,15 @@
 package com.example.pfi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.pfi.classes.Panier;
 import com.example.pfi.classes.Produit;
@@ -19,9 +24,18 @@ public class MenuPage extends AppCompatActivity {
     }
 
     /**
-     * Créer le recyclerview pour créer la liste des produits à vendre
+     * Class to set fonts
      *
      */
+    private void setFontMenu(){
+        TextView textView = findViewById(R.id.title_menu);
+        Typeface customTypeface = ResourcesCompat.getFont(this, R.font.stardew_font);
+
+        textView.setTypeface(customTypeface);
+        textView.setTextColor(ContextCompat.getColor(this,R.color.white));
+        textView.setTextSize(5,5);
+        textView.setGravity(Gravity.CENTER);
+    }
 
 
 
