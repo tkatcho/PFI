4a8331d2b9dc0bb9df2ab8603d04245fa933b1c1
elianarg <137467171+elianagonzalez01@users.noreply.github.com>
2023-12-09 21:29:36 -0500

    ss

--- app/src/main/java/com/example/pfi/MenuPage.java
+++ app/src/main/java/com/example/pfi/MenuPage.java
@@ -3,6 +3,8 @@ package com.example.pfi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -14,6 +16,9 @@ import android.widget.TextView;
 import com.example.pfi.classes.Panier;
 import com.example.pfi.classes.Produit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuPage extends AppCompatActivity {
     Panier panier = new Panier();
 
@@ -21,6 +26,15 @@ public class MenuPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
+
+        RecyclerView RecyclerView = findViewById(R.id.recycler);
+
+        List<Produit> items =  new ArrayList<Produit>();
+        items.add(new Produit("Breakfast",2,"description",R.drawable.breakfast,1)); // for now
+
+
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.setAdapter(new Adapter(getApplicationContext(),));
     }
 
 
