08b9b48fba39b5968d3d622e66151966e908e994
elianarg <137467171+elianagonzalez01@users.noreply.github.com>
2023-12-10 16:15:30 -0500

    yup

--- app/src/main/java/com/example/pfi/MenuPage.java
+++ app/src/main/java/com/example/pfi/MenuPage.java
@@ -35,22 +35,7 @@ public class MenuPage extends AppCompatActivity {
 
 
         RecyclerView.setLayoutManager(new LinearLayoutManager(this));
-       // RecyclerView.setAdapter(new Adapter(getApplicationContext(),));
-    }
-
-    /**
-     * Class to set fonts
-     *
-     */
-    private void setFontMenu(){
-        TextView textView = findViewById(R.id.title_menu);
-        Typeface customTypeface = ResourcesCompat.getFont(this, R.font.stardew_font);
-
-        textView.setTypeface(customTypeface);
-        textView.setTextColor(ContextCompat.getColor(this,R.color.white));
-        textView.setTextSize(5,5);
-        textView.setGravity(Gravity.CENTER);
-//test
+       // RecyclerView.setAdapter(new Adapter(getApplicationContext(),)); // possible erreur
     }
 
 
