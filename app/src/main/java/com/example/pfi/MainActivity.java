c8bb4d06fdffdcd12ebb80303fe02d3a45abcc4d
elianarg <137467171+elianagonzalez01@users.noreply.github.com>
2023-12-10 13:36:19 -0500

    yup

--- app/src/main/java/com/example/pfi/MainActivity.java
+++ app/src/main/java/com/example/pfi/MainActivity.java
@@ -90,6 +90,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // login
+
+    
     private void loginFadeAnim(){
         View loginForm = findViewById(R.id.loginForm);
 
--- app/src/main/java/com/example/pfi/classes/ViewHolder.java
+++ app/src/main/java/com/example/pfi/classes/ViewHolder.java
@@ -1,22 +1,24 @@
-//package com.example.pfi.classes;
-//
-//import android.view.View;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.example.pfi.R;
-//
-//public class ViewHolder extends RecyclerView.ViewHolder {
-//
-//   public ImageView imageView;
-//    public TextView nameView , priceView;
-//    public ViewHolder(@NonNull View itemView) {
-//        super(itemView);
-//        imageView = itemView.findViewById(R.id.imageView);
-//        nameView = itemView.findViewById(R.id.name);
-//        priceView = itemView.findViewById(R.id.price);
-//    }
-//}
+package com.example.pfi.classes;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pfi.R;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+   public ImageView imageView;
+    public TextView nameView , priceView;
+    public View linearLayout;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = itemView.findViewById(R.id.imageView);
+        nameView = itemView.findViewById(R.id.name);
+        priceView = itemView.findViewById(R.id.price);
+    }
+}
